6f324bb6eeab4db15feaf59fb87230bbd0e17577
mamayie <mappiah319@gmail.com>
2022-05-11 14:15:30 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'malik';
+
+
+function upperCaseCustomerName(){
+
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+  
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'meera';
+}
+
+console.log(changeLeastFavoriteCustomer());
